285ccd825d46864575a908a848eca06583f84537
Monmon <syu24059@gmail.com>
2024-10-01 15:24:22 +0900

    a little change of env settings

--- backend/addCandidates.js
+++ backend/addCandidates.js
@@ -1,24 +1,25 @@
+require("dotenv").config();
 const { ethers } = require("ethers");
 const Voting = require("../voting-dapp/src/Voting.json"); // 导入合约的 ABI 和网络信息
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
-    "http://localhost:7545"
+    //process.env.SEPOLIA_RPC_URL
+    process.env.GANACHE_URL
   );
 
-  const privateKey =
-    "0xb9c1fd6b4a7d758cf0dcf77295ab143088753c3e48da50737727d6c8aef104e3";
+  //const privateKey = process.env.PRIVATE_KEY;
+  const privateKey = process.env.GANACHE_KEY;
   const wallet = new ethers.Wallet(privateKey, provider);
 
   const contract = new ethers.Contract(
     Voting.networks[1337].address,
+    //Voting.networks[11155111].address,
     Voting.abi,
     wallet
   );
 
-  await contract.addCandidate("Alice");
-  await contract.addCandidate("Bob");
-  await contract.addCandidate("Cathy");
+  await contract.addCandidate("Dennis");
   console.log("Candidates added");
 }
 
--- foundry-voting/script/DeployVoting.s.sol
+++ foundry-voting/script/DeployVoting.s.sol
@@ -16,3 +16,17 @@ contract DeployVoting is Script {
         vm.stopBroadcast();
     }
 }
+
+/**
+ * ##### sepolia
+ * ✅  [Success]Hash: 0x5d3b8ea4cb6cc63aa7c6711e2d32dda0469d997a3f3e6671be5431eb4881ef25
+ * Contract Address: 0x79Bd6DCb15Ca2b0B0abe5EAa1A7bCBD2d5d379E7
+ * Block: 6489965
+ * Paid: 0.003410351733437102 ETH (597617 gas * 5.706584206 gwei)
+ *
+ * ##### ganache
+ * ✅  [Success]Hash: 0xa6410687546ff12dd87986f952be05bd37eb99db78b6b7b008cc4d7e92ea9d3b
+ * Contract Address: 0x878a049752a5f1dB7a62e2cCa2B282C229226dF4
+ * Block: 119
+ * Paid: 0.001792371240177714 ETH (597457 gas * 3.000000402 gwei)
+ */
